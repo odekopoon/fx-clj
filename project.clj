c4d3d82dabddd682dd279a47b2a1f9ea3d2cffcd
Koji Yusa <odekopoon@gmail.com>
2016-02-22 01:01:41 +0900

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,15 +3,17 @@
   :url "https://github.com/aaronc/fx-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-                 [garden "1.2.1"]
-                 [potemkin "0.3.8"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [garden "1.3.1"]
+                 [potemkin "0.4.3"]
                  [org.tobereplaced/lettercase "1.0.0"]
                  [freactive.core "0.2.0-alpha1"]]
   :java-source-paths ["src"]
   :javac-options ["-Xlint:unchecked"]
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
   :profiles
   {:dev
     {:plugins
-      [[codox "0.8.10"]]}})
+      [[codox "0.8.10"]]}
+   :uberjar {:aot :all}})
